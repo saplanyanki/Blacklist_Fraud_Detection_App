9d4c019b7e07233c6de4ad206d81ad6f8db03f6c
efreedman56 <elibfreedman56@gmail.com>
2023-04-05 11:01:45 -0400

    Validation testing

---
+++ app/validation_test.py
@@ -0,0 +1,29 @@
+import sys
+sys.path.append("XBNet")
+
+from training_utils import validate
+from run import Data
+import pandas as pd
+import numpy
+import joblib
+import torch
+from torch.utils.data import Dataset, DataLoader
+
+
+validation_df = pd.read_csv("val.csv")
+v3 = joblib.load("instance/v3.pkl")
+
+
+val_x = validation_df.drop('is_fraud', axis=1)
+val_y = validation_df['is_fraud']
+
+
+val_x = val_x.to_numpy()
+val_y = val_y.to_numpy()
+
+
+validation_df = DataLoader(Data(val_x, val_y), batch_size=32)
+
+
+criterion = torch.nn.BCEWithLogitsLoss()
+validate(v3, validation_df, criterion, 5)
